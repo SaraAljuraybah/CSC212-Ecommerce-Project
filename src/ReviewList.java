e1271afc80611962a6f4ebdae1caddc089eea859
haya30 <haya9aziz@gmail.com>
2025-11-14 12:39:39 +0300

    Update ReviewList.java

--- src/ReviewList.java
+++ src/ReviewList.java
@@ -34,11 +34,7 @@ public class ReviewList implements ListInterface<Reviews>{
         return total / size;
     }
     
- // ------------------------------------------------------------
- // Query #1: Get all reviews by a specific customer (reviewer)
- // Time Complexity: O(n)  -- linear scan through the linked list
- // Space Complexity: O(1)
- // ------------------------------------------------------------
+
  public void getReviewsByCustomer(int customerId) {
      if (head == null) {
          System.out.println("No reviews available.");
@@ -64,15 +60,10 @@ public class ReviewList implements ListInterface<Reviews>{
          System.out.println("This customer has not submitted any reviews yet.");
      }
  }
-//&&&&&&&&&&&&&&&&&
-//------------------------------------------------------------
-//Helper: does this list contain a review by a given customer?
-//Time: O(n), Space: O(1)
-//------------------------------------------------------------
-//------------------------------------------------------------
+
 
 public boolean hasReviewByCustomer(int customerId) {
-  Node cur = head;                 // your inner node type
+  Node cur = head;                
   while (cur != null) {
       Reviews r = cur.data;
       if (r != null && r.getReviewer() != null
@@ -84,50 +75,51 @@ public boolean hasReviewByCustomer(int customerId) {
   return false;
 }
 
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&    
+ 
     
 
 	@Override
 	public Reviews searchById(int id) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
+		
 		return size ;
 	}
 	
-	//*****************************
+
 	public void addReview(int customerId, int productId, int rating, String comment,
             CustomerList customers, ProductList products) {
-// نبحث عن العميل
+
 Customers customer = customers.searchById(customerId);
 if (customer == null) {
-System.out.println("❌ Customer not found with ID " + customerId);
+System.out.println(" Customer not found with ID " + customerId);
 return;
 }
 
-// نبحث عن المنتج
+
 Products product = products.searchById(productId);
 if (product == null) {
-System.out.println("❌ Product not found with ID " + productId);
+System.out.println(" Product not found with ID " + productId);
 return;
 }
 
-// ننشئ التقييم الجديد
+
 Reviews review = new Reviews(rating, comment, customer,product );
 
-// نربط التقييم بالمنتج
+
 product.addReview(review);
 
-// نضيفه في القائمة العامة
+
 add(review);
 
-System.out.println("✅ Review added successfully by " + customer.getName() +
+System.out.println(" Review added successfully by " + customer.getName() +
              " for product: " + product.getName());
 }
-//*****************************************************
+
 }
+
 
